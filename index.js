6faade01111d2f5da812d7eda503cfc0dd27a274
sebastian2698 <sebl0002@stud.ek.dk>
2026-09-09 22:01:56 +0200

    done

--- index.js
+++ index.js
@@ -14,6 +14,34 @@ const date = document.querySelector("#choosedate");
 const weathercheck = document.querySelector("#weather");
 const klok = new Date();
 
+const wwCodes = {
+  0: "clearsky_day.png",
+  1: "fair_day.png",
+  2: "partlycloudy_day.png",
+  3: "cloudy.png",
+  45: "fog.png",
+  48: "fog.png",
+  51: "lightrain.png",
+  53: "lightrain.png",
+  55: "lightrain.png",
+  56: "lightsleet.png",
+  57: "lightsleet.png",
+  61: "lightrain.png",
+  63: "rain.png",
+  65: "heavyrain.png",
+  66: "lightsleet.png",
+  67: "lightsleet.png",
+  71: "lightsnow.png",
+  73: "snow.png",
+  75: "heavysnow.png",
+  77: "lightsnow.png",
+  80: "lightrainshowers_day.png",
+  81: "rainshowers_day.png",
+  82: "heavyrainshowers_day.png",
+  85: "lightsnowshowers_day.png",
+  86: "heavysnowshowers_day.png",
+  95: "rainandthunder.png",
+};
 document.getElementById("klokken").innerHTML = klok;
 
 createTask_btn.addEventListener("click", createTask);
@@ -76,6 +104,8 @@ function prepareObjects(jsonData, task) {
 
   // Vejeret gemmes til opgaven som API hører til
   task.weather = weatherText;
+
+  task.weatherImage = wwCodes[weathercode];
   // renderList gør at funktionen kører igen, hvergang data opdateres
   renderList();
 }
@@ -90,8 +120,12 @@ function renderList() {
     // Hvis opgaven markeres som fuldført sendes den ned til .fuldfort classen fra HTML
     if (task.taskDone) {
       li.innerHTML = `
-      <p>${task.taskTxt}</p> <p>${task.taskDate}</p> <p>${task.weather}</p>
-<button class="regrettask" title="Fortryd handling">✓</button>
+      <p>${task.taskTxt}</p> <p>${task.taskDate}</p> <p>${(task.outdoor && task.weather) || `<span class="weatherresult">Ikke udendørs</span>`}</p>
+
+
+
+
+      <button class="regrettask" title="Fortryd handling">✓</button>
     `;
 
       const regrettask = li.querySelector(".regrettask");
@@ -110,10 +144,17 @@ function renderList() {
            
       <input type="checkbox"  title="Fuldfør opgave" ${task.taskDone ? "checked" : ""}
         ${task.outdoor && task.weather === "Regnvejr" ? "disabled" : ""}>
+        
         <p class="opgavetekst">${task.taskTxt}</p>
          <p class= "datotekst">${task.taskDate}</p>
      
- ${task.outdoor ? `<span class="weatherresult">${task.weather || "Henter vejr..."}</span>` : `<span class="weatherresult">Ikke udendørs</span>`}
+ ${
+   task.outdoor
+     ? `<span class="weatherresult">${task.weather || "Henter vejr..."}
+ ${task.weatherImage ? `<img src="png/${task.weatherImage}" alt"${task.weather}">` : ""}
+ </span>`
+     : `<span class="weatherresult">Ikke udendørs</span>`
+ }
 
 <button class="deletetask" title="Slet task">X</button>
 `;
